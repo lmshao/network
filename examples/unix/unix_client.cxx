8ea5c7d917091acff63baf159625894fa472dbc8
SHAO Liming <lmshao@163.com>
2025-07-24 14:02:16 +0800

    Add Unix domain socket examples

---
+++ examples/unix/unix_client.cxx
@@ -0,0 +1,63 @@
+#include "unix_client.h"
+
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include "iclient_listener.h"
+
+bool gExit = false;
+class EchoClientListener : public IClientListener {
+public:
+    void OnReceive(int fd, std::shared_ptr<DataBuffer> data) override
+    {
+        std::string msg((char *)data->Data(), data->Size());
+        std::cout << "Received from server: " << msg << std::endl;
+    }
+
+    void OnClose(int fd) override
+    {
+        std::cout << "Connection closed: fd=" << fd << std::endl;
+        gExit = true;
+    }
+
+    void OnError(int fd, const std::string &reason) override
+    {
+        std::cout << "Error: fd=" << fd << ", reason=" << reason << std::endl;
+        gExit = true;
+    }
+};
+
+int main()
+{
+    std::string socketPath = "/tmp/unix_echo.sock";
+    auto client = UnixClient::Create(socketPath);
+    auto listener = std::make_shared<EchoClientListener>();
+    client->SetListener(listener);
+    if (!client->Init()) {
+        std::cerr << "Client init failed!" << std::endl;
+        return 1;
+    }
+    if (!client->Connect()) {
+        std::cerr << "Client connect failed!" << std::endl;
+        return 1;
+    }
+
+    char sendbuf[1024];
+    printf("Input:\n----------\n");
+    while (!gExit && fgets(sendbuf, sizeof(sendbuf), stdin)) {
+        if (client->Send(sendbuf, strlen(sendbuf))) {
+            std::cout << "Send scuccess, length: " << strlen(sendbuf) << std::endl;
+        } else {
+            std::cout << "Send error" << std::endl;
+        }
+
+        memset(sendbuf, 0, sizeof(sendbuf));
+        printf("Input:\n----------\n");
+    }
+
+    client->Close();
+    return 0;
+}
---
+++ examples/unix/unix_echo_server.cxx
@@ -0,0 +1,69 @@
+#include <unistd.h>
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "iserver_listener.h"
+#include "session.h"
+#include "unix_server.h"
+
+class EchoServerListener : public IServerListener {
+public:
+    void OnAccept(std::shared_ptr<Session> session) override
+    {
+        std::cout << "----------" << std::endl;
+        std::cout << "pid: " << std::this_thread::get_id() << std::endl;
+        std::cout << "OnAccept: from " << session->ClientInfo() << std::endl;
+        std::cout << "----------" << std::endl;
+    }
+    void OnReceive(std::shared_ptr<Session> session, std::shared_ptr<DataBuffer> data) override
+    {
+        std::cout << "----------" << std::endl;
+        std::cout << "pid: " << std::this_thread::get_id() << std::endl;
+        std::cout << "OnReceive " << data->Size() << " bytes from " << session->ClientInfo() << std::endl;
+        std::cout << (char *)data->Data() << std::endl;
+        std::cout << "----------" << std::endl;
+        if (session->Send(data)) {
+            std::cout << "send echo data ok." << std::endl;
+        } else {
+            std::cout << "send echo data failed." << std::endl;
+        }
+        std::cout << "----------" << std::endl;
+    }
+    void OnClose(std::shared_ptr<Session> session) override
+    {
+        std::cout << "----------" << std::endl;
+        std::cout << "pid: " << std::this_thread::get_id() << std::endl;
+        std::cout << "OnClose: from " << session->ClientInfo() << std::endl;
+        std::cout << "----------" << std::endl;
+    }
+    void OnError(std::shared_ptr<Session> session, const std::string &reason) override
+    {
+        std::cout << "----------" << std::endl;
+        std::cout << "pid: " << std::this_thread::get_id() << std::endl;
+        std::cout << "OnError: '" << reason << "' from " << session->ClientInfo() << std::endl;
+        std::cout << "----------" << std::endl;
+    }
+};
+
+int main()
+{
+    std::string socketPath = "/tmp/unix_echo.sock";
+    auto server = UnixServer::Create(socketPath);
+    auto listener = std::make_shared<EchoServerListener>();
+    server->SetListener(listener);
+    if (!server->Init()) {
+        std::cerr << "Server init failed!" << std::endl;
+        return 1;
+    }
+    if (!server->Start()) {
+        std::cerr << "Server start failed!" << std::endl;
+        return 1;
+    }
+    std::cout << "UNIX Echo Server started at " << socketPath << std::endl;
+    while (true) {
+        sleep(1);
+    }
+    return 0;
+}
